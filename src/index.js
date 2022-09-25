a179bb93d08ea0d3152f4a8a5579f2b6daf873d9
Kayo.io <kayorenatocontato@gmail.com>
2022-09-25 20:17:27 -0300

    chore: add function checkExistsUserAccount

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const cpf = request.cpf;
+
+  const userExists = users.some((user) => { user.id === cpf })
+
+  if (userExists) return response.status(404).json({ error: "User Exists!" })
+
+  return next();
+
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post('/users', checksExistsUserAccount, (request, response) => {
+
+  const user = {
+    id: uuidv4(),
+    name: request.body,
+    username: request.body,
+    todos: []
+  }
+
+  users.push(user)
+
+  return response.json(user)
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
