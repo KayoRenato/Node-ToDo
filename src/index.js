e54720ff8704d76abd76c7805a2e4b8f1e0f8096
Kayo.io <kayorenatocontato@gmail.com>
2022-10-01 16:37:58 -0300

    chore: add create todo and test

--- src/index.js
+++ src/index.js
@@ -11,44 +11,68 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  const cpf = request.cpf;
+  const { username } = request.headers;
 
-  const userExists = users.some((user) => { user.id === cpf })
+  const user = users.find(user => user.username == username);
 
-  if (userExists) return response.status(404).json({ error: "User Exists!" })
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" }); x
+  }
+
+  request.user = user;
 
   return next();
 
 }
 
-app.post('/users', checksExistsUserAccount, (request, response) => {
+app.get('/users', (request, response) => {
+  return response.json(users);
+})
+
+app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
-  const usernameAlreadyExists = users.some((user) => user.username == username)
+  const usernameAlreadyExists = users.some((user) => user.username === username);
 
   if (usernameAlreadyExists) {
-    return response.status(400).json({ error: "Username already exists!" })
-  }
+    return response.status(400).json({ error: "Username Exists!" });
+  };
 
   const user = {
     id: uuidv4(),
     name,
     username,
     todos: []
-  }
+  };
 
-  users.push(user)
+  users.push(user);
 
-  return response.json(user)
+  return response.status(201).json(user);
 
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {todos}  = request.user;
+
+  return response.json(todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  const newToDo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    create_at: new Date()
+  }
+
+  user.todos.push(newToDo);
+
+  return response.status(201).json(newToDo);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
